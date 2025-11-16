fa8f23420d5322489723862e581bee8422fc071f
AL - ARAFAT (  YEASH  ) <149018537+researcher-program@users.noreply.github.com>
2025-11-16 11:54:47 +0600

    Add files via upload
    This repo is : PHP Syntax 
           and  
                Run in Web browser { http://localhost/PHP_Developer/Index.php }.

---
+++ PHP-Syntax_Index-1.php
@@ -0,0 +1,25 @@
+<!-- PHP Basic Syntax : 
+ 
+ <?php 
+
+?> 
+-->
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title> PHP_Tutorial-1 </title>
+</head>
+<body>
+    <h1> Hi, This Series is Going to Start PHP Tutorial Absolute to Beginner to Experts and Coverup Basic To Advaced ... </h1>
+
+<?php 
+
+// Here is the body part of coding to write inside the PHP
+echo "Welcome To PHP Developers";
+
+?> 
+</body>
+</html>
